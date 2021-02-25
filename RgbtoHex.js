f001d26c1d59661f55dcaef9569eb839c645372a
josh_CA <53126628+joshmg-CA@users.noreply.github.com>
2021-02-24 19:07:35 -0600

    Update RgbtoHex.js

--- RgbtoHex.js
+++ RgbtoHex.js
@@ -1,1 +1,10 @@
 
+const RgbtoHex = (rgb)=>{
+  let getNumbers = rgb.replace(/(\brgb)?([()])/g,"");    
+  let numbers = getNumbers.split(",");    
+  let hexa0 = parseInt(numbers[0]).toString(16);
+  let hexa1 = parseInt(numbers[1]).toString(16);
+  let hexa2 = parseInt(numbers[2]).toString(16);
+  
+  return `#${hexa0+hexa1+hexa2}`;    
+}
